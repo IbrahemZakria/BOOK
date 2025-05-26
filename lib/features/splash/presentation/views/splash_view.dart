c320c6d52aca71a5cac5f00c5bec191ce6b54384
IbrahemZakria <https://github.com/IbrahemZakria>
2025-05-26 13:22:02 +0200

    رسالة الالتزام

---
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,29 @@
+import 'package:book/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(AssetsData.logo, width: 200, height: 200),
+            const SizedBox(height: 20),
+            const Text(
+              'Welcome to Book App',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
