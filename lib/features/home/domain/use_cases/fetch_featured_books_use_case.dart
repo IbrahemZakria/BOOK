6c62750d7c3ed22e9d93cf57d161d363f619307f
IbrahemZakria <https://github.com/IbrahemZakria>
2025-06-22 05:38:13 +0200

    FetchFeaturedBooksUseCase

---
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:book/core/error/failures.dart';
+import 'package:book/core/use_cases/no_param_uses_case.dart';
+import 'package:book/features/home/domain/entities/book_entity.dart';
+import 'package:book/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchFeaturedBooksUseCase extends UsesCase<List<BookEntity>> {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase(this.homeRepo);
+
+  @override
+  Future<Either<Failures, List<BookEntity>>> call() {
+    return homeRepo.fetchFeaturedBooks();
+  }
+}
